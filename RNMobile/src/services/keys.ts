55732018d4d58a2074caca9d2cf18fca5b9df233
annaniksweb <aghasi89@gmail.com>
2023-11-17 18:13:59 +0400

    chnage api-url

--- RNMobile/src/services/keys.ts
+++ RNMobile/src/services/keys.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Config from 'react-native-config';
 
 class Keys {
-  static API_URL = 'http://192.168.27.34:3000/';
+  static API_URL = 'http://164.92.173.189:3000/';
 }
 
 export const setTauRusApiAuthorizationHeader = (token: string | null) => {
